8e58a7059433a840e5546a6f39f18032f6065b42
bianca-tee <112151662+bianca-tee@users.noreply.github.com>
2022-12-21 10:35:51 +1100

    Added codeMapping for animated weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,12 +3,35 @@ import ReactAnimatedWeather from "react-animated-weather";
 import "./TemperatureImage.css";
 
 export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "CLOUDY",
+    "03n": "CLOUDY",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "SLEET",
+    "10n": "SLEET",
+    "11d": "SLEET",
+    "11n": "SLEET",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
+  };
+
   return (
-    <ReactAnimatedWeather
-      icon="CLEAR_DAY"
-      color="orange"
-      size={64}
-      animate={true}
-    />
+    <div className="main-icon">
+      <ReactAnimatedWeather
+        icon={codeMapping[props.icon]}
+        color="black"
+        size={80}
+        animate={true}
+      />
+    </div>
   );
 }
