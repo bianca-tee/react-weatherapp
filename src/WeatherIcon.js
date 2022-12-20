026e1bcabd08a623dd07fe82fc5411905529e683
bianca-tee <112151662+bianca-tee@users.noreply.github.com>
2022-12-21 10:00:14 +1100

    Installed, imported React Animated Weather
    Added into weathericon.js with default data

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,6 +1,14 @@
 import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
 import "./TemperatureImage.css";
 
 export default function WeatherIcon(props) {
-  return <p>Hello</p>;
+  return (
+    <ReactAnimatedWeather
+      icon="CLEAR_DAY"
+      color="orange"
+      size={64}
+      animate={true}
+    />
+  );
 }
