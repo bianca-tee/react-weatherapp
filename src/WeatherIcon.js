0edc57a6ad1048ec6c6d0ae9c8135d9845de8ae8
bianca-tee <112151662+bianca-tee@users.noreply.github.com>
2022-12-23 21:34:00 +1100

    Removed Saerch.css (duplicate), renamed to WeatherIcon.css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
-import "./TemperatureImage.css";
+import "./WeatherIcon.css";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
